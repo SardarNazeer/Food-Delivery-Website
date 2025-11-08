940939a9eb3848f0f0e752e69092ba5fac2ceda0
Sardar Nazeer <157596245+SardarNazeer@users.noreply.github.com>
2025-11-09 03:39:50 +0500

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "http:food-delivery-website-production-c28b.up.railway.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
